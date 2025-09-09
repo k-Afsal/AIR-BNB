e60fe807baa5935cb5f420fbb7ca8fe78bd1b5e6
k-Afsal <apsal.k2004@gmail.com>
2025-09-09 17:38:17 +0530

    j

--- AirBnb-Backend/obj/Debug/net9.0/AirBnb-Backend.AssemblyInfo.cs
+++ AirBnb-Backend/obj/Debug/net9.0/AirBnb-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirBnb-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cb7361032dfb77787f530c21a3d8910879369a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirBnb-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirBnb-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
